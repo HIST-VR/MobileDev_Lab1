d7a4ee853f8b9b3dcd4a6a0861097cd8529b87af
HIST-VR <48255627+HIST-VR@users.noreply.github.com>
2020-10-11 21:49:06 +0300

    committed

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Vladyslav Romacnhenko on 10/11/20.
+//  Copyright © 2020 Vladyslav Romacnhenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
